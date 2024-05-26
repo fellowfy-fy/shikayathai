c6705c3a0b9bf9b512c687b6f3ab76b4adbd59e2
terrxrmachine <so3.workspace@gmail.com>
2024-05-25 20:07:34 -0400

    test comment

--- shikayathai/ref_shikaya/webapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, Complaint, Company, UserCompany
-
-admin.site.register(UserProfile)
-admin.site.register(Complaint)
-admin.site.register(Company)
-admin.site.register(UserCompany)
-
-# Register your models here.
