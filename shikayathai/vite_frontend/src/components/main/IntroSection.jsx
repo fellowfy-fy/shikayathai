1c48921dba2ccb27e7edd2bf70fd2fc4d96124ea
terrxrmachine <so3.workspace@gmail.com>
2024-05-28 18:19:08 -0400

    починил intro section

--- shikayathai/vite_frontend/src/components/main/IntroSection.jsx
+++ shikayathai/vite_frontend/src/components/main/IntroSection.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { BRAND_NAME } from "../../constants";
-
+import "../../styles/IntroSection.css"
 
 const IntroSection = () => (
     <section className="intro-section">
       <div className="intro-content">
-        <div className="intro-title-wrapper">
-          <b className="intro-title">Welcome to {BRAND_NAME}!</b>
-        </div>
-        <div className="intro-text">
+        <h1 className="intro-title">Welcome to {BRAND_NAME}!</h1>
+        <p className="intro-text">
           Here, we make sure your complaints get heard and fixed, fast. File
           Your Complaint Easily:
+        </p>
+        <div className="intro-steps">
+          <div className="intro-step">
+            <span className="intro-step-number">1</span>
+            <div className="intro-step-detail">Describe Your Issue: Let us know what went wrong.</div>
+          </div>
+          <div className="intro-step">
+            <span className="intro-step-number">2</span>
+            <div className="intro-step-detail">Submit: We handle the details, contacting the company and making your case public.</div>
+          </div>
+          <div className="intro-step">
+            <span className="intro-step-number">3</span>
+            <div className="intro-step-detail">See Results: Track your complaint's journey to resolution.</div>
+          </div>
         </div>
       </div>
-      <button className="cta-button" onClick={onFileComplaintClick}>
-        {DESCRIBE_YOUR_ISSUE}
-      </button>
     </section>
-  );
+);
 
-export default IntroSection
+export default IntroSection;
