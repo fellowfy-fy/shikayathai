cf854adf7ae6e4046a46d32dae616f77407830e4
terrxrmachine <so3.workspace@gmail.com>
2024-06-03 17:52:41 -0400

    пофиксил отображение цифр в intro section

--- shikayathai/vite_frontend/src/components/main/IntroSection.jsx
+++ shikayathai/vite_frontend/src/components/main/IntroSection.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import { BRAND_NAME } from "../../constants";
-import "../../styles/Main.css"
+import "../../styles/Main.css";
 
 const IntroSection = () => (
-    <section className="intro-section">
-      <div className="intro-content">
-        <h1 className="intro-title">Welcome to {BRAND_NAME}!</h1>
-        <p className="intro-text">
-          Here, we make sure your complaints get heard and fixed, fast. File
-          Your Complaint Easily:
-        </p>
-        <div className="intro-steps">
-          <div className="intro-step">
-            <span className="intro-step-number">1</span>
-            <div className="intro-step-detail">Describe Your Issue: Let us know what went wrong.</div>
+  <section className="intro-section">
+    <div className="intro-content">
+      <h1 className="intro-title">Welcome to {BRAND_NAME}!</h1>
+      <p className="intro-text">
+        Here, we make sure your complaints get heard and fixed, fast. File
+        Your Complaint Easily:
+      </p>
+      <div className="intro-steps">
+        <div className="intro-step">
+          <span className="intro-step-number">1</span>
+          <div className="intro-step-detail">
+            <h3 className="step-header">Describe Your Issue:</h3>
+            <p>Let us know what went wrong.</p>
           </div>
-          <div className="intro-step">
-            <span className="intro-step-number">2</span>
-            <div className="intro-step-detail">Submit: We handle the details, contacting the company and making your case public.</div>
+        </div>
+        <div className="intro-step">
+          <span className="intro-step-number">2</span>
+          <div className="intro-step-detail">
+            <h3 className="step-header">Submit:</h3>
+            <p>We handle the details, contacting the company and making your case public.</p>
           </div>
-          <div className="intro-step">
-            <span className="intro-step-number">3</span>
-            <div className="intro-step-detail">See Results: Track your complaint's journey to resolution.</div>
+        </div>
+        <div className="intro-step">
+          <span className="intro-step-number">3</span>
+          <div className="intro-step-detail">
+            <h3 className="step-header">See Results:</h3>
+            <p>Track your complaint's journey to resolution.</p>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 );
 
 export default IntroSection;
