1b7895938e71c58e5dffc6671d96969ed0b752fc
fellowfy <126517749+fyfellow@users.noreply.github.com>
2024-06-14 09:05:04 +0300

    Сделал ComplaintPage

--- shikayathai/vite_frontend/src/components/RecentRequestSection/RecentReqeustSection.jsx
+++ shikayathai/vite_frontend/src/components/RecentRequestSection/RecentReqeustSection.jsx
@@ -1,10 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import FrameComponent from "../FrameComponent/FrameComponent.jsx";
 import api from "../../api/axios";
 import { Link } from "react-router-dom";
 
 function RecentRequestSection() {
   const [requests, setRequests] = useState([]);
+  const scrollContainerRef = useRef(null);
+
+
+  
+  const scroll = (direction) => {
+    if (scrollContainerRef.current) {
+      const { current: scrollContainer } = scrollContainerRef;
+      if (direction === 'left') {
+        scrollContainer.scrollBy({ left: -200, behavior: 'smooth' });
+      } else {
+        scrollContainer.scrollBy({ left: 200, behavior: 'smooth' });
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchRequests = async () => {
@@ -19,21 +33,37 @@ function RecentRequestSection() {
     fetchRequests();
   }, []);
 
+
+
   return (
-    <div className="text-center px-8 pt-10 bg-white">
+    <div className="text-center px-8 pt-10 bg-white relative">
       <h2 className="text-4xl font-bold text-[#001A45] font-unbounded mb-4">
         Recent Requests
       </h2>
       <Link to="complaints" className="text-[#001A45]">
-        Watch all 
+        Watch all
       </Link>
       {requests.length > 0 ? (
-        <div className="overflow-x-auto">
-          <div className="flex space-x-8">
-            {requests.map((request, index) => (
-              <FrameComponent key={index} data={request} />
-            ))}
+        <div className="relative">
+          <button
+            onClick={() => scroll('left')}
+            className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-md z-10"
+          >
+            &#8249;
+          </button>
+          <div className="overflow-x-auto scrollbar-hide" ref={scrollContainerRef}>
+            <div className="flex space-x-8">
+              {requests.map((request, index) => (
+                <FrameComponent key={index} data={request} className="min-w-[16rem]"/>
+              ))}
+            </div>
           </div>
+          <button
+            onClick={() => scroll('right')}
+            className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-md z-10"
+          >
+            &#8250;
+          </button>
         </div>
       ) : (
         <p>No recent requests available.</p>
--- shikayathai/vite_frontend/src/pages/ComplaintPage/ComplaintPage.jsx
+++ shikayathai/vite_frontend/src/pages/ComplaintPage/ComplaintPage.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../api/axios.jsx";
 import SuccessBanner from "./SuccBanner/SuccessBanner.jsx";
 import Comments from "./CommentSection/Comments.jsx";
-import "./ComplaintSection.css";
 import profilePlaceholder from "../../assets/profilePlaceholder.png";
 import imagePlaceholder from "../../assets/imagePlaceholder.jpg";
 import share from "../../assets/share.svg";
@@ -15,6 +14,18 @@ import "react-medium-image-zoom/dist/styles.css";
 const ComplaintDetail = () => {
   const { id } = useParams();
   const [complaint, setComplaint] = useState(null);
+
+  
+  const scrollContainerRef = useRef(null);
+
+  const scrollLeft = () => {
+    scrollContainerRef.current.scrollBy({ left: -154, behavior: "smooth" });
+  };
+
+  const scrollRight = () => {
+    scrollContainerRef.current.scrollBy({ left: 154, behavior: "smooth" });
+  };
+
 
   useEffect(() => {
     api
@@ -41,66 +52,95 @@ const ComplaintDetail = () => {
       : [];
 
   return (
-    <div className="complaint-page">
+    <div className="p-6 bg-white rounded-lg shadow-md mx-auto">
       <SuccessBanner id={id} />
-      <div className="user-info">
+      <div className="flex items-center my-6">
         <img
           src={
-            `http://localhost:8000${complaint.author_userpic}` ||
-            profilePlaceholder
+            complaint.author_userpic
+              ? `http://localhost:8000${complaint.author_userpic}`
+              : profilePlaceholder
           }
           alt="User avatar"
-          className="user-avatar"
+          className="w-12 h-12 rounded-full mr-4"
         />
-        <div className="user-details">
-          <h4 className="user-name">{complaint.author_name}</h4>
-          <p className="user-company">{complaint.company_name}</p>
+        <div>
+          <h4 className="text-lg font-bold text-[#001A45]">
+            {complaint.author_name}
+          </h4>
+          <p className="text-sm text-gray-600">{complaint.company_name}</p>
         </div>
       </div>
-      <h1 className="complaint-title">{complaint.title}</h1>
-      <div className="complaint-content">
-        <div className="image-gallery">
-          <Zoom key={1}>
-            <img src={mainImage} alt="Main" className="main-image" />
+      <h1 className="text-5xl font-bold text-[#001A45] pl-3 whitespace-pre-wrap break-all">
+        {complaint.title}
+      </h1>
+      <div className="flex gap-6 items-start">
+        <div className="flex flex-col items-start mt-5">
+          <Zoom>
+            <img
+              src={mainImage || imagePlaceholder}
+              alt="Main"
+              className="w-[640px] h-[640px] object-cover rounded-lg mb-6"
+            />
           </Zoom>
-          <div className="thumbnails">
+          
+          <div className="relative">
+            <button
+              onClick={scrollLeft}
+              className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white text-gray-600 p-2 rounded-full shadow-md z-10"
+            >
+              &lt;
+            </button>
+          <div className="flex overflow-x-auto space-x-2 w-[640px]" ref={scrollContainerRef} style={{ scrollBehavior: "smooth" }}>
             {thumbnails.map((photo, index) => (
-              <Zoom key={index}>
+              <span key={index}>
+              <Zoom >
+              <span className="flex w-[154px] h-[154px]">
                 <img
                   src={photo.image || imagePlaceholder}
                   alt={`Thumbnail ${index}`}
-                  className="thumbnail-image"
+                  className="w-[154px] h-[154px] object-cover rounded-lg shrink-0"
                 />
-              </Zoom>
+
+                </span>
+              </Zoom></span>
             ))}
           </div>
+          
+          <button
+              onClick={scrollRight}
+              className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white text-gray-600 p-2 rounded-full shadow-md"
+            >
+              &gt;
+            </button>
         </div>
-        <div className="complaint-details">
-          <p className="complaint-description">{complaint.description}</p>
-          <div className="attachments">
+        </div>
+        <div className="flex-1">
+          <p className="mb-4 mt-4 text-gray-800 whitespace-pre-wrap break-all">
+            {complaint.description}
+          </p>
+          <div className="mt-4 text-blue-500">
             {complaint.documents &&
               complaint.documents.map((doc, index) => (
-                <div className="attachment" key={index}>
+                <div key={index} className="mt-4">
                   <a href={doc.file} target="_blank" rel="noopener noreferrer">
                     {decodeURIComponent(doc.file.split("/").pop())}
                   </a>
                 </div>
               ))}
           </div>
-          <div className="timestamp">
+          <p className="text-sm text-gray-600 mt-4">
             Posted on {new Date(complaint.created_at).toLocaleDateString()}
-          </div>
-          <div className="action-buttons">
-            <button className="support-button">
-              <img src={support} alt="Support" />
-              Support
+          </p>
+          <div className="flex justify-start gap-2.5 mt-4">
+            <button className="bg-gray-200 text-[#001A45] py-2.5 px-5 rounded text-sm flex items-center gap-2">
+              <img src={support} alt="Support" className="w-4 h-4" /> Support
             </button>
-            <button className="share-button">
-              <img src={share} alt="Share" />
-              Share
+            <button className="bg-gray-200 text-[#001A45] py-2.5 px-5 rounded text-sm flex items-center gap-2">
+              <img src={share} alt="Share" className="w-4 h-4" /> Share
             </button>
-            <button className="resolved-button">
-              <img src={resolved} alt="Mark as resolved" />
+            <button className="bg-gray-200 text-[#001A45] py-2.5 px-5 rounded text-sm flex items-center gap-2">
+              <img src={resolved} alt="Mark as resolved" className="w-4 h-4" />{" "}
               Mark as resolved
             </button>
           </div>
