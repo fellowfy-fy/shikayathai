d5937b717f1f632fafeb2ad4aaaea57d92b9cbe8
fellowfy <126517749+fyfellow@users.noreply.github.com>
2024-06-25 22:23:00 +0300

    HeroSection Fontsize fix

--- shikayathai/vite_frontend/src/components/HeroSection/HeroSection.jsx
+++ shikayathai/vite_frontend/src/components/HeroSection/HeroSection.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { useModal } from '../../context/ModalContext';
-import FileComplaintForm from '../FileComplaintForm/FileComplaintForm.jsx';
+import { useModal } from "../../context/ModalContext";
+import FileComplaintForm from "../FileComplaintForm/FileComplaintForm.jsx";
 import man from "../../assets/man.png";
-import mansmall from "../../assets/mansmall.png"; 
+import mansmall from "../../assets/mansmall.png";
 
 const HeroSection = () => {
   const { showModal } = useModal();
@@ -17,8 +17,8 @@ const HeroSection = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   const getImageSrc = () => {
@@ -31,8 +31,8 @@ const HeroSection = () => {
   return (
     <section className="bg-[#FFD2BE] w-full rounded-bl-[30px] rounded-br-[30px] px-5 sm:px-24 md:px-30 pt-6">
       <div className="flex flex-col sm:flex-row items-start justify-between">
-        <div className="text-left max-w-lg">
-          <h1 className="flex flex-col text-[32px] sm:text-[128px] font-unbounded font-bold text-[#001A45] leading-tight">
+        <div className="text-left max-w-lg lg:pb-[106px] z-10">
+          <h1 className="flex flex-col text-[32px] lg:text-[96px] 2xl:text-[128px] font-unbounded font-bold text-[#001A45] leading-tight">
             <span>FREE</span>
             <span>Complaint</span>
             <span>Platform</span>
@@ -40,16 +40,16 @@ const HeroSection = () => {
           <p className="text-[20px] sm:text-[24px] font-inter font-medium text-[#001A45] mt-4">
             Turning grievances into wins, guaranteed
           </p>
-          <button 
+          <button
             className="mt-5 hover:bg-[#C9FF57] text-[#001A45] bg-[#B5F62B] active:bg-[#A9E922] sm:w-[312px] w-[100%] h-[55px] rounded-[12px] font-inter font-medium text-[18px] transition-all mx-auto sm:mx-0 md:mx-0"
             onClick={handleFileComplaintClick}
           >
             File a complaint
           </button>
         </div>
-        <img 
-          className={`${windowWidth < 1830 ? 'max-w-[600px]' : 'max-w-[816px]'} w-full h-auto object-contain mt-auto`}
-          src={getImageSrc()} 
+        <img
+          className={`${windowWidth < 1830 ? "max-w-[600px]" : "max-w-[816px]"} w-full h-auto object-contain mt-auto`}
+          src={getImageSrc()}
           alt="Happy man holding a card"
         />
       </div>
