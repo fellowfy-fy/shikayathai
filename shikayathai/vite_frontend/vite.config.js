0385c4ee2865f23f714a159324f00b2cba1910c1
Alexander Eroshkin <alexander.eroshkin@gmail.com>
2024-08-03 19:45:31 +0300

    gtm

--- shikayathai/vite_frontend/vite.config.js
+++ shikayathai/vite_frontend/vite.config.js
@@ -5,9 +5,15 @@ import { VitePluginRadar } from 'vite-plugin-radar'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePluginRadar({
+    enableDev: true,
     // Google Analytics tag injection
-    analytics: {
-      id: 'G-XXXXX',
-    },
+    analytics: [
+      {
+        id: 'G-LPXH1KHVNR',
+      }],
+    gtm: [
+      {
+        id: 'GTM-M78GFCC9',
+      }]
   })],
 })
